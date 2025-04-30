3a3a28515a50f3d71f8783f550de59f6bcfeb257
GuiFBrito <75683002+GuiFBrito@users.noreply.github.com>
2025-04-30 05:56:28 +0000

    projeto pronto xadrez novato

--- xadrez.c
+++ xadrez.c
@@ -1,38 +1,79 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
-
-    printf("desafio xadrez");
 
 
 
+int main(){
+
+int i = 1;    
+int torre,rainha,bispo;
+char escolhaPeca;
 
 
-    return 0;
+printf("########## Bem bindo ao jogo de xadrez #########\n");
+printf("#### nesse jogo você vai escolher com qual pec usar e seus movimentos #######\n");
+
+printf("escolha uma peça para mover:\n");
+printf("R - Rainha\n");
+printf("T - Torre\n");
+printf("B - Bispo\n");
+scanf("%c", &escolhaPeca);
+
+
+switch (escolhaPeca){
+
+case 'R':
+case 'r':
+    /* code */
+    printf("###quantas casas você ira mover a rainha:###\n");
+    scanf("%d", &rainha); 
+    
+    while (i <= rainha){
+        printf("esquerda\n");
+
+        i++;
+    }
+
+    break;
+case 'B':
+case 'b':
+
+    printf("###Quantas casas voce ira mover o Bispo###\n");
+    scanf("%d", &bispo);
+
+    do
+    {
+        printf("Cima, direita\n");
+        i++;
+
+    } while (i <= bispo);
+    
+break;
+
+case 'T':
+case 't':   
+
+    printf("###Quantas casas voce quer mover a sua torre:\n");
+    scanf("%d", &torre);
+
+        for (i = 1; i <= torre; i++){
+
+            printf("Direita\n");
+        }
+
+
+    break;
+
+
+
+default:
+
+    printf("#####Comando inválido#####\n");
+    break;
+
+
+}
+
+return 0;
+
 }
